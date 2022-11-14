a63bd6fe1e663fcf57a2cb9f132ecf5d7b557cea
Ethan Ling <ethanhling@gmail.com>
2022-11-14 15:46:05 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+Ethan Ling
+ethan.ling95@myhunter.cuny.edu
+3/7/21
+tong yi 
+im studying for the mid term
+212
+char32_t31
+3
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    int total = 0;
+    
+    
+    char sign;
+    cin >> total;
+    
+    while (cin >> sign >> num)
+    {
+        if (sign == '^')
+        { 
+            total = num * num;
+        }
+    }
+    {
+        if (sign == '+')
+        { 
+            total += num; 
+        }
+    }
+    {
+        else if (sign == '-')
+        {
+            total -= num;
+        }
+    }
+    {
+        else if (sign == ';')
+        {
+            cout << total << endl;
+        }
+    }
+    
+
+
+
+
+
